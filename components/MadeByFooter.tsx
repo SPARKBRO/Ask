8f9419c9e15a0880b9c57c2c03e0bdb5243bfa15
Midhun <103011794+SPARKBRO@users.noreply.github.com>
2025-03-05 14:36:26 +0530

    Update MadeByFooter.tsx

--- components/MadeByFooter.tsx
+++ components/MadeByFooter.tsx
@@ -1,25 +1,7 @@
-import { Github, Instagram } from "lucide-react";
-
 export default function Footer() {
   return (
-    <footer className="w-full py-4 bg-gray-900 text-white flex items-center justify-center gap-2">
-      <p>Made with ❤️ by Jerin</p>
-      <a
-        href="https://www.instagram.com/jerin.javascript"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="hover:text-pink-500 transition"
-      >
-        <Instagram size={20} />
-      </a>
-      <a
-        href="https://github.com/jerinjacob007/ask-a-date"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="hover:text-gray-500 transition"
-      >
-        <Github size={20} />
-      </a>
+    <footer className="w-full py-4 bg-gray-900 text-white flex items-center justify-center">
+      <p>Made with ❤️ by SPARK</p>
     </footer>
   );
 }
